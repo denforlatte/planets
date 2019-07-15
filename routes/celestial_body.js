34eb1d73bfdb8b9199bd9cd61a788ccd29fd8d60
Danny Wilkins <danny@chippingscottage.eclipse.co.uk>
2019-07-15 12:37:02 +0100

    Added GET /celestial_body/:id route

--- routes/celestial_body.js
+++ routes/celestial_body.js
@@ -6,6 +6,23 @@ const authentication = require('../middleware/authentication');
 // Models
 const CelestialBody = require('../models/CelestialBody');
 
+// @ROUTE   GET /celestial_body/:id
+// @DESC    Retrieve data on a celestial body
+// @ACCESS  Public
+router.get('/:id', async (req, res) => {
+  try {
+  const celestialBody = await CelestialBody.findById(req.params.id);
+
+  if (!celestialBody) return res.status(400).json({msg: 'Celestial body not found.'});
+
+  return res.json(celestialBody);
+  } catch (error) {
+    console.error(error.message);
+    return res.status(500).send('Server error');
+  }
+  
+});
+
 // @ROUTE   POST /celestial_body
 // @DESC    Add a new celestial body. Not yet used on frontend but useful for testing and adding the first bodies.
 // @ACCESS  Private
@@ -13,11 +30,12 @@ router.post('/', [ authentication, [
   // Additional validation on top of frontend validation for more flexibility and reliability
   check('name', 'Name is required').not().isEmpty(),
   check('type', 'Type is required').not().isEmpty(),
+  check('image_path', 'Image Path is required').not().isEmpty(),
   check('mass', 'Mass is required').not().isEmpty(),
   check('diameter', 'Diameter is required').not().isEmpty(),
   check('density', 'Density is required').not().isEmpty(),
   check('surface_gravity', 'Gravity is required').not().isEmpty(),
-  check('number_of_moons', 'Moons is required').not().isEmpty(),
+  check('number_of_moons', 'Moons is required').not().isEmpty()
 ]], async (req, res) => {
   const errors = validationResult(req)
   if(!errors.isEmpty()) {
@@ -26,12 +44,13 @@ router.post('/', [ authentication, [
   
   // Create new object from req and save to collection.
   try {
-    const { name, type, star_system, distance_sun, mass, diameter, density, surface_gravity, number_of_moons, interesting_facts } = req.body;
+    const { name, type, image_path, star_system, distance_sun, mass, diameter, density, surface_gravity, number_of_moons, interesting_facts } = req.body;
 
     // Create an intermediate object in case optional inputs are not sent.
     let celestialBodyFields = {
       name,
       type,
+      image_path,
       mass,
       diameter,
       density,
