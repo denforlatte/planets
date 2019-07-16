2fd7054ebd68725d8dbb57cef8e18267ac35779a
Danny Wilkins <danny@chippingscottage.eclipse.co.uk>
2019-07-16 17:07:44 +0100

    Configured server for deployment

--- server.js
+++ server.js
@@ -3,6 +3,7 @@ require('dotenv').config();
 
 const express = require('express');
 const connectToDatabase = require('./database');
+const path = require('path');
 
 // Initialise express with JSON parser
 const app = express();
@@ -12,13 +13,19 @@ app.use(express.json({extended: false}));
 connectToDatabase();
 
 // Server actions
-app.get('/', (req, res) => res.send('API online.'));
-
 // @NOTE  Added routes now in case more functionality needs to be added later.
 app.use('/celestial_body', require('./routes/celestial_body'));
 app.use('/star_system', require('./routes/star_system'));
 app.use('/authentication', require('./routes/authentication'));
 app.use('/images', express.static('public'));
+
+// Serve client files
+if (process.env.NODE_ENV === 'production') {
+  app.use(express.static('client/build'));
+  app.get('*', (req, res) => {
+    res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+  });
+}
 
 // If .PORT (production) doesn't exist, use the local .env's port
 const PORT = process.env.PORT || process.env.DEV_SERVER_PORT;
